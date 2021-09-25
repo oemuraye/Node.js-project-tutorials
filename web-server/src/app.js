1c345f8b3f16c5d27efd53946878db74cb52b859
oemuraye <oemuraye@gmail.com>
2021-09-25 19:08:14 +0100

    linking webpages to server

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,25 +1,34 @@
-const express = require('express')
+const path = require("path");
+const express = require("express");
 
-const app = express()
+const app = express();
+const publicDirectoryPath = path.join(__dirname, "../public");  //access the HTML page
 
-app.get('', (req, res) => {
-    res.send('Hello express!')
-})
 
-app.get('/help', (req, res) => {
-  res.send("Help page");
+
+
+// app.set("views", path.join(__dirname, "views"));
+// app.engine("html", require("hbs").renderFile);
+app.set("view engine", "hbs")  //having a template like header&footer for pages
+app.use(express.static(publicDirectoryPath));
+
+
+// This is how to create html tags/serve JSON, etc
+// app.get("/about", (req, res) => {
+//   res.send("<h1>About page</h1>");
+// });
+
+app.get("", (req, res) => {
+  res.render("index");
 });
 
-app.get('/about', (req, res) => {
-  res.send("About page");
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About Me",
+    name: "Andrew Mead",
+  });
 });
-
-app.get('/weather', (req, res) => {
-  res.send("Weather page");
-});
-
-
 
 app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
-})
+  console.log("Server is up on port 3000.");
+});
