b51a211d1c566fba3130947b89ee4635962abe26
oemuraye <oemuraye@gmail.com>
2021-09-27 06:38:23 +0100

    accessing Api/server

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,1 +1,19 @@
 console.log('Client file')
+
+fetch("https://puzzle.mead.io/puzzle").then((response) => {
+    response.json().then((data) => {
+        console.log(data)
+    })
+})
+
+fetch("http://localhost:3000/weather?address=!").then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            return console.log(data.error)   
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+        }
+    })
+    
+})
