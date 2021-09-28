283cf75ada95324e8349f6b140652198e5798469
oemuraye <oemuraye@gmail.com>
2021-09-28 05:50:15 +0100

    delected unsed js function

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -1,10 +1,10 @@
 console.log('Client file')
 
-fetch("https://puzzle.mead.io/puzzle").then((response) => {
-    response.json().then((data) => {
-        console.log(data)
-    })
-})
+// fetch("https://puzzle.mead.io/puzzle").then((response) => {
+//     response.json().then((data) => {
+//         console.log(data)
+//     })
+// })
 
 
 
