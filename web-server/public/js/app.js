fd89d3cd021a048be3ec3a33a870768c4b1328ab
oemuraye <oemuraye@gmail.com>
2021-09-28 05:45:26 +0100

    finished weather app

--- web-server/public/js/app.js
+++ web-server/public/js/app.js
@@ -6,14 +6,34 @@ fetch("https://puzzle.mead.io/puzzle").then((response) => {
     })
 })
 
-fetch("http://localhost:3000/weather?address=!").then((response) => {
-    response.json().then((data) => {
-        if (data.error) {
-            return console.log(data.error)   
-        } else {
-            console.log(data.location)
-            console.log(data.forecast)
-        }
-    })
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.querySelector('#message-1')
+const messageTwo = document.querySelector('#message-2')
+
+// messageOne.textContent = 'from js'
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const location = search.value
+
+    messageOne.textContent = 'loading...'
+    messageTwo.textContent = ' ' || null
     
+    fetch("http://localhost:3000/weather?address=" + location + "").then((response) => {
+      response.json().then((data) => {
+        if (data.error) {
+          return (
+              messageOne.textContent = 'Error...!',
+              messageTwo.textContent = data.error
+              )
+        } else {
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        }
+      });
+    });
 })
