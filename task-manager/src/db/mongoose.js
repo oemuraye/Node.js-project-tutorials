af685283e4870f8ac1ef3858ab5e522682027594
oemuraye <oemuraye@gmail.com>
2021-10-06 19:59:51 +0100

    still learning mongoose tools

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require('validator')
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true
@@ -7,20 +8,74 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 
 const User = mongoose.model("User", {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+          throw new Error('Email is invalid')
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error('Age must be a positive number')
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    minlength: 7,
+    trim: true,
+    validate(value) {
+      if (value.toLowerCase().includes('password')) {
+        throw new Error('Password should not contain "password"')
+      }
+    }
   }
 });
 
-const me = new User({
-  name: "Pius",
-  age: "26"
-});
+// const me = new User({
+//   name: "  Pius ",
+//   email: 'MYEMAIL@EMAD',
+//   password: 'phone@43!'
+// });
 
-me.save().then(() => {
-    console.log(me);
-  }).catch((error) => {
-    console.log("Error!", error);
-  });
+// me.save().then(() => {
+//     console.log(me);
+//   }).catch((error) => {
+//     console.log("Error!", error);
+//   });
+
+const Tasks = mongoose.model('Tasks', {
+  description: {
+    type: String,
+    required: true,
+    trim: true
+  },
+  completed: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const task = new Tasks({
+  description: "Go to school",
+  
+})
+
+task.save().then(() => {
+  console.log(task)
+}).catch((error) => {
+  console.log('Error!', error)
+})
