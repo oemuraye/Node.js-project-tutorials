cc939ff69fa35e01a96494611967a27efff06c65
oemuraye <oemuraye@gmail.com>
2021-10-26 17:18:07 +0100

    finished my task app lesson project

--- task-manager/src/db/mongoose.js
+++ task-manager/src/db/mongoose.js
@@ -1,6 +1,81 @@
-const mongoose = require("mongoose")
+const mongoose = require("mongoose");
+// const validator = require('validator')
 
-mongoose.connect(process.env.MONGODB_URL, {
+mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true
   // useCreateIndex: true
-})
+});
+
+// const User = mongoose.model("User", {
+//   name: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//   },
+//   email: {
+//     type: String,
+//     required: true,
+//     trim: true,
+//     lowercase: true,
+//     validate(value) {
+//       if (!validator.isEmail(value)) {
+//           throw new Error('Email is invalid')
+//       }
+//     }
+//   },
+//   age: {
+//     type: Number,
+//     default: 0,
+//     validate(value) {
+//       if (value < 0) {
+//         throw new Error('Age must be a positive number')
+//       }
+//     }
+//   },
+//   password: {
+//     type: String,
+//     required: true,
+//     minlength: 7,
+//     trim: true,
+//     validate(value) {
+//       if (value.toLowerCase().includes('password')) {
+//         throw new Error('Password should not contain "password"')
+//       }
+//     }
+//   }
+// });
+
+// const me = new User({
+//   name: "  Pius ",
+//   email: 'MYEMAIL@EMAD',
+//   password: 'phone@43!'
+// });
+
+// me.save().then(() => {
+//     console.log(me);
+//   }).catch((error) => {
+//     console.log("Error!", error);
+//   });
+
+// const Tasks = mongoose.model('Tasks', {
+//   description: {
+//     type: String,
+//     required: true,
+//     trim: true
+//   },
+//   completed: {
+//     type: Boolean,
+//     default: false
+//   }
+// })
+
+// const task = new Tasks({
+//   description: "Go to school",
+  
+// })
+
+// task.save().then(() => {
+//   console.log(task)
+// }).catch((error) => {
+//   console.log('Error!', error)
+// })
